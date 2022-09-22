4de9639d98e905bf8be28c03c42046f811bd0752
Admin <834354487@qq.com>
2022-09-22 10:57:24 +0800

    IU-2021.3 <Admin@DESKTOP-HPBBT7Q Create baseRefactoring.xml, easy-code-setting.xml, IntelliLang.xml, ide.general.xml, mybatisx.xml, print.xml, project.default.xml, advancedSettings.xml, diff.xml, Enum.java, Interface.java, yiiguxing.translation.xml, shared-indexes.xml, Default.xml, MybatisConfig.xml, find.xml, ignored-suggested-plugins.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, androidEditors.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, databaseDrivers.xml, maven.wrapper.mapping.xml, github.xml, Windows Proper Redo.xml, Mapper.xml, laf.xml, Default.xml, docker.xml, Class.java, jdk.table.xml, encoding.xml, vcs.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author XWanLi
+ * @since ${DATE}
+ * @version 1.0
+ */
+public class ${NAME} {
+}
